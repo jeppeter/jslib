a777e7afc2530fd46aad3c4662f3c42eec67db3a
jeppeter <jeppeter@gmail.com>
2016-03-29 17:16:07 +0800

    to add tst command into repo

---
+++ test/tstcmd/tstcmdline.js
@@ -0,0 +1,18 @@
+var yargs = require('yargs');
+var util = require('util');
+
+var argv = yargs.count('verbose')
+    .alias('verbose', 'v')
+    .usage(util.format('Usage %s [OPTIONS] file', process.argv[1]))
+    .default({
+        file: __filename,
+        interactive: 'no'
+    })
+    .help('h')
+    .alias('h', 'help')
+    .alias('file', 'F')
+    .demand(1)
+    .alias('i', 'interactive')
+    .argv;
+
+console.log(argv);
