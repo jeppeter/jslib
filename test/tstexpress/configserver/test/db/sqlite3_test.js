04405d0c174dc0877d4d2ed748ad82fe9d4312dd
jeppeter <jeppeter@gmail.com>
2016-04-04 12:08:09 +0800

    to add run sqlite3

--- test/tstexpress/configserver/test/db/sqlite3_test.js
+++ test/tstexpress/configserver/test/db/sqlite3_test.js
@@ -1,18 +1,20 @@
 var sqlite3 = require('sqlite3');
 var util = require('util');
 var argv = require('yargs')
-    .demand(1)
     .help('h')
-    .string('create')
-    .alise('create', 'C')
-    .default('C', 'user')
+    .string('sql')
+    .aliase('sql', 'S')
+    .default('S', 'select * from user')
+    .string('database')
+    .alise('database', 'D')
+    .default('D', 'test.db')
     .alias('h', 'help')
     .count('verbose')
     .alias('verbose', 'v')
-    .usage(util.format('Usage %s [OPTIONS] file', process.argv[1]))
+    .usage(util.format('Usage %s [OPTIONS] sql', process.argv[1]))
     .argv;
 
-var databasename = argv._[0];
+var databasename = argv.D;
 var sqldb = sqlite3.DataBase(databasename, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (err) {
     'use strict';
     if (err) {
@@ -20,6 +22,16 @@ var sqldb = sqlite3.DataBase(databasename, sqlite3.OPEN_READWRITE | sqlite3.OPEN
         process.exit(3);
     }
 });
-if (argv.C) {
 
+if (argv.verbose > 0) {
+    sqldb.verbose();
 }
+
+sqldb.run(argv.S, function (err) {
+    'use strict';
+    if (err) {
+        console.error('run(%s) error(%s)', argv.S, err);
+    } else {
+        console.log('run %s succ', argv.S);
+    }
+});
