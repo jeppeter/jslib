c5fd4e934f84f7d17cc7d0c9d3fd78d63c88c653
jeppeter <jeppeter@gmail.com>
2016-04-09 13:10:12 +0800

    to make the object

--- test/tstoo/lib/Person.js
+++ test/tstoo/lib/Person.js
@@ -2,20 +2,30 @@ function Person(name, addr) {
     'use strict';
     this.name = name;
     this.addr = addr;
-    this.GetAddr = function () {
-        return this.addr;
-    };
-    this.GetName = function () {
-        return this.name;
-    };
-    this.SetName = function (val) {
-        this.name = val;
-    };
-
-    this.SetAddr = function (val) {
-        this.addr = val;
-    };
 }
+
+Person.prototype.GetAddr = function () {
+    'use strict';
+    return this.addr;
+};
+
+Person.prototype.SetAddr = function (addr) {
+    'use strict';
+    this.addr = addr;
+    return;
+};
+
+Person.prototype.GetName = function () {
+    'use strict';
+    return this.name;
+};
+
+
+Person.prototype.SetName = function (name) {
+    'use strict';
+    this.name = name;
+    return;
+};
 
 module.exports.NewPerson = function (name, addr) {
     'use strict';
