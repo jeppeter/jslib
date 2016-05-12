8f20deeb431a09a0b7c1eac50c59f263e74917f7
jeppeter <jeppeter@gmail.com>
2016-05-12 17:37:44 +0800

    to make main post for the next use

--- test/tstgrab/hkexnewsmain_post.js
+++ test/tstgrab/hkexnewsmain_post.js
@@ -1,6 +1,7 @@
 var tracelog = require('../../tracelog');
 var cheerio = require('cheerio');
 var util = require('util');
+var qs = require('querystring');
 
 function createHkexNewsMainPost(options) {
     'use strict';
@@ -20,8 +21,9 @@ function createHkexNewsMainPost(options) {
     hknews.options.sel_tier_1 = '-2';
     hknews.options.sel_DocTypePrior2006 = '-1';
     hknews.options.sel_tier_2_group = '-2';
-    hknews.options.ddlTierTwo = '59,1,7';
-    hknews.options.ddlTierTwoGroup = '19,5';
+    hknews.options.sel_tier_2 = '-2';
+    hknews.options.ddlTierTwo = '59%2C1%2C7';
+    hknews.options.ddlTierTwoGroup = '19%2C5';
     hknews.options.txtKeyWord = '';
     hknews.options.rdo_SelectDateOfRelease = 'rbManualRange';
     hknews.options.sel_DateOfReleaseFrom_d = hknews.options.startdate.substring(6, 8);
@@ -56,10 +58,64 @@ function createHkexNewsMainPost(options) {
         hknews.options.txt_stock_name = setopt.stockname;
     }
 
+    hknews.make_post_data = function (inputctrl) {
+        var postdata;
+        postdata = '';
+        postdata += '__VIEWSTATE=';
+        postdata += qs.escape(inputctrl.attribs.value);
+        postdata += '&__VIEWSTATEENCRYPTED=&ctl00%24txt_today=';
+        postdata += util.format('%s&', hknews.options.txt_today);
+        postdata += 'ctl00%24hfStatus=';
+        postdata += util.format('%s&', hknews.options.hfStatus);
+        postdata += 'ctl00%24hfAlert=';
+        postdata += util.format('%s&', hknews.options.hfAlert);
+
+        postdata += 'ctl00%24txt_stock_code=';
+        postdata += util.format('%s&', hknews.options.txt_stock_code);
+        postdata += 'ctl00%24txt_stock_name=';
+        postdata += util.format('%s&', hknews.options.txt_stock_name);
+        postdata += 'ctl00%24rdo_SelectDocType=';
+        postdata += util.format('%s&', hknews.options.rdo_SelectDocType);
+        postdata += 'ctl00%24sel_tier_1=';
+        postdata += util.format('%s&', hknews.options.sel_tier_1);
+        postdata += 'ctl00%24sel_DocTypePrior2006=';
+        postdata += util.format('%s&', hknews.options.sel_DocTypePrior2006);
+        postdata += 'ctl00%24sel_tier_2_group=';
+        postdata += util.format('%s&', hknews.options.sel_tier_2_group);
+        postdata += 'ctl00%24sel_tier_2=';
+        postdata += util.format('%s&', hknews.options.sel_tier_2);
+        postdata += 'ctl00%24ddlTierTwo=';
+        postdata += util.format('%s&', hknews.options.ddlTierTwo);
+        postdata += 'ctl00%24ddlTierTwoGroup=';
+        postdata += util.format('%s&', hknews.options.ddlTierTwoGroup);
+        postdata += 'ctl00%24txtKeyWord=';
+        postdata += util.format('%s&', hknews.options.txtKeyWord);
+        postdata += 'ctl00%24rdo_SelectDateOfRelease=';
+        postdata += util.format('%s&', hknews.options.rdo_SelectDateOfRelease);
+        postdata += 'ctl00%24sel_DateOfReleaseFrom_d=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseFrom_d);
+        postdata += 'ctl00%24sel_DateOfReleaseFrom_m=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseFrom_m);
+        postdata += 'ctl00%24sel_DateOfReleaseFrom_y=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseFrom_y);
+        postdata += 'ctl00%24sel_DateOfReleaseTo_d=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseTo_d);
+        postdata += 'ctl00%24sel_DateOfReleaseTo_m=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseTo_m);
+        postdata += 'ctl00%24sel_DateOfReleaseTo_y=';
+        postdata += util.format('%s&', hknews.options.sel_DateOfReleaseTo_y);
+        postdata += 'ctl00%24sel_defaultDateRange=';
+        postdata += util.format('%s&', hknews.options.sel_defaultDateRange);
+        postdata += 'ctl00%24rdo_SelectSortBy=';
+        postdata += util.format('%s', hknews.options.rdo_SelectSortBy);
+        return postdata;
+    };
+
     hknews.post_handler = function (err, worker, next) {
         var parser;
         var inputs, i, input;
         var findinput;
+        var postdata;
         if (err) {
             /*if we have nothing to do*/
             next(true, err);
@@ -96,7 +152,15 @@ function createHkexNewsMainPost(options) {
             return;
         }
 
-        /*now we find the input ,so we should all things*/
+        /*now we find the input ,so we should all things we should put post data*/
+        postdata = hknews.make_post_data(findinput);
+        worker.parent.post_queue(worker.url, {
+            hkexnewspaper: true,
+            reuse: true,
+            reqopt: {
+                body: postdata
+            }
+        });
 
         next(false, null);
     };
