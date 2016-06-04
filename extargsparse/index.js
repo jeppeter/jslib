3eda31c66c39cb2add53bedb84cfd87ee62c7316
jeppeter <jeppeter@gmail.com>
2016-06-04 09:22:58 +0800

    to make load_command_line several functions

--- extargsparse/index.js
+++ extargsparse/index.js
@@ -25,6 +25,130 @@ function NewExtArgsParse(opt) {
     if (opt.priority !== undefined && opt.priority !== null) {
         parser.priority = opt.priority;
     }
+
+    self.check_flag_insert = function (keycls, curparser) {
+        var idx;
+        var curflag;
+        if (curparser) {
+            for (idx = 0; idx < curparser.flags.length; idx += 1) {
+                curflag = curparser.flags[idx];
+                if (curflag.flagname !== '$' && keycls.flagname !== '$') {
+                    if (curflag.optdest === keycls.optdest) {
+                        return false;
+                    }
+                } else if (curflag.flagname === keycls.flagname) {
+                    return false;
+                }
+            }
+            curparser.flags.push(keycls);
+        } else {
+            for (idx = 0; idx < self.flags.length; idx += 1) {
+                curflag = self.flags[idx];
+                if (curflag.flagname !== '$' && keycls.flagname !== '$') {
+                    if (curflag.optdest === keycls.optdest) {
+                        return false;
+                    }
+                } else if (curflag.flagname === keycls.flagname) {
+                    return false;
+                }
+            }
+            self.flags.push(keycls);
+        }
+        return true;
+    };
+
+    self.check_flag_insert_mustsucc = function (keycls, curparser) {
+        var inserted;
+        var errstr;
+
+        inserted = self.check_flag_insert(keycls, curparser);
+        if (!inserted) {
+            errstr = util.format('(%s) has inserted', keycls.optdest);
+            throw new Error(errstr);
+        }
+        return inserted;
+    };
+    self.load_command_line_args = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert(keycls, curparser);
+    };
+    self.load_command_line_boolean = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+
+    self.load_command_line_array = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+
+    self.load_command_line_float = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+
+    self.load_command_line_int = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+    self.load_command_line_string = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+
+    self.get_subparser_inner = function (keycls) {
+        var idx;
+        var cursubparser;
+        for (idx = 0; idx < self.subparsers.length; idx += 1) {
+            cursubparser = self.subparsers[idx];
+            if (keycls.cmdname === cursubparser.cmdname) {
+                return cursubparser;
+            }
+        }
+
+        cursubparser = {};
+        cursubparser.flags = [];
+        cursubparser.cmdname = keycls.cmdname;
+        cursubparser.keycls = keycls;
+        self.subparsers.push(cursubparser);
+        return cursubparser;
+    };
+
+    self.load_command_line_command = function (prefix, keycls, curparser) {
+        var errstr;
+        if (curparser) {
+            errstr = util.format('can not make (%s) in another subcommand (%s)', keycls.command, curparser.cmdname);
+            throw new Error(errstr);
+        }
+        if (keycls.typename !== 'Object') {
+            errstr = util.format('(%s) not  object value', keycls.cmdname);
+            throw new Error(errstr);
+        }
+
+        prefix = prefix;
+        curparser = self.get_subparser_inner(keycls);
+        self.load_command_line_inner(keycls.cmdname, keycls.value, curparser);
+        return true;
+    };
+
+    self.load_command_line_prefix = function (prefix, keycls, curparser) {
+        var errstr;
+
+        if (typeof keycls.value !== 'object') {
+            errstr = util.format('(%s) not object value', keycls.prefix);
+            throw new Error(errstr);
+        }
+        prefix = prefix;
+        self.load_command_line_inner(keycls.prefix, keycls.value, curparser);
+        return true;
+    };
+
+    self.load_command_line_count = function (prefix, keycls, curparser) {
+        prefix = prefix;
+        return self.check_flag_insert_mustsucc(keycls, curparser);
+    };
+
+
     self.load_command_line_inner = function (prefix, opt, curparser) {
         var keys;
         var curkey, curval;
@@ -52,6 +176,18 @@ function NewExtArgsParse(opt) {
     self.load_command_line = function (cmdopt) {
         return self.load_command_line_inner('', cmdopt, null);
     };
+
+    self.set_command_line_self_args = function () {
+        var curkey;
+        self.subparsers.forEach(function (elm) {
+            curkey = keyparse.KeyParser(elm.cmdname, '$', '*', true);
+            self.load_command_line_args(elm.cmdname, curkey, elm);
+        });
+        curkey = keyparse.KeyParser('', '$', '*', true);
+        self.load_command_line_args('', curkey, null);
+        return;
+    };
+
 
     self.load_command_line_string = function (cmdstr) {
         var cmdopt;
