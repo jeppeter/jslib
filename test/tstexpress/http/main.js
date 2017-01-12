79454709714737259178887c494711ad4eef1eed
jeppeter <jeppeter@gmail.com>
2017-01-12 13:16:39 +0800

    to coding for large file drain

--- test/tstexpress/http/main.js
+++ test/tstexpress/http/main.js
@@ -3,12 +3,15 @@ var tracelog = require('../../../tracelog');
 var extargsparse = require('extargsparse');
 var parser, args;
 var express = require('express');
-//var fs = require('fs');
+var util = require('util');
+var fs = require('fs');
 var commandline = `
 {
     "port|p" : 447,
     "key|k" : "server.pem",
-    "cert|c" : "servercert.pem"
+    "cert|c" : "servercert.pem",
+    "kernel|K" : null,
+    "initrd|I" : null
 }
 `;
 var app = express();
@@ -19,6 +22,14 @@ args = parser.parse_command_line();
 
 
 tracelog.set_args(args);
+
+if (args.kernel === null ||
+        args.vmlinuz === null) {
+    tracelog.error('no kernel or vmlinuz specified');
+    process.exit(3);
+}
+
+
 
 app.get('/', function (req, res) {
     'use strict';
@@ -34,8 +45,46 @@ app.get('/kernel', function (req, res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     req = req;
     tracelog.info('call from %s', ip);
-    res.header('Content-type', 'text/html');
-    return res.end('<h1>Kernel</h1>');
+    /*now we should make coding for more than handing*/
+    fs.fstat(args.kernel, function (err, stats) {
+        var parameters = '';
+        var rstream;
+        if (err !== null) {
+            tracelog.error('can not stat (%s) error(%s)', args.kernel, err);
+            res.status(401);
+            return res.end();
+        }
+
+
+        parameters += util.format('ISCSI_INITIATOR=com.bingte.iscsi.client1');
+        res.writeHead(200, {
+            'Content-Type': 'application/octet-stream',
+            'Content-Length': util.format('%d', (stats.size + parameters.length + 1))
+        });
+
+        rstream = fs.createReadStream(args.kernel, 'UTF-8');
+        rstream.on('data', function(chunk) {
+            res.write(chunk);
+        });
+        rstream.on('error', function(err2) {
+
+        });
+        rstream.on('end' , function() {
+
+        });
+
+
+    });
+    return;
+});
+
+app.get('/initrd', function (req, res) {
+    'use strict';
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    req = req;
+    tracelog.info('call from %s', ip);
+    res.download(args.initrd);
+    return;
 });
 
 
--- test/tstexpress/https/main.js
+++ test/tstexpress/https/main.js
@@ -4,21 +4,33 @@ var extargsparse = require('extargsparse');
 var parser, args;
 var express = require('express');
 var fs = require('fs');
+var seckey = '';
+var seccert = '';
+var app = express();
 var commandline = `
 {
     "port|p" : 447,
     "key|k" : "server.pem",
-    "cert|c" : "servercert.pem"
+    "cert|c" : "servercert.pem",
+    "kernel|K" : null,
+    "initrd|I" : null
 }
 `;
-var app = express();
+
 parser = extargsparse.ExtArgsParse();
 tracelog.init_args(parser);
 parser.load_command_line_string(commandline);
 args = parser.parse_command_line();
 
-
 tracelog.set_args(args);
+
+
+if (args.kernel === null ||
+        args.vmlinuz === null) {
+    tracelog.error('no kernel or vmlinuz specified');
+    process.exit(3);
+}
+
 
 app.get('/', function (req, res) {
     'use strict';
@@ -34,14 +46,41 @@ app.get('/kernel', function (req, res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     req = req;
     tracelog.info('call from %s', ip);
-    res.header('Content-type', 'text/html');
-    return res.end('<h1>Kernel</h1>');
+    res.download(args.kernel);
+    return;
 });
 
-tracelog.info('list on %d', args.port);
-https.createServer({
-    key: fs.readFileSync(args.key),
-    cert: fs.readFileSync(args.cert)
-},
-        app).listen(args.port);
+app.get('/initrd', function (req, res) {
+    'use strict';
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    req = req;
+    tracelog.info('call from %s', ip);
+    res.download(args.initrd);
+    return;
+});
+
+
+fs.readFile(args.key, function (err, data) {
+    'use strict';
+    if (err !== null) {
+        tracelog.error('can not read %s error(%s)', args.key, err);
+        process.exit(3);
+        return;
+    }
+    seckey = data;
+    fs.readFile(args.cert, function (err2, data) {
+        if (err2 !== null) {
+            tracelog.error('can not read %s error(%s)', args.cert, err2);
+            process.exit(3);
+            return;
+        }
+        seccert = data;
+        /*now we should create server*/
+        tracelog.info('listen on %d', args.port);
+        https.createServer({
+            key: seckey,
+            cert: seccert
+        }, app).listen(args.port);
+    });
+});
 
