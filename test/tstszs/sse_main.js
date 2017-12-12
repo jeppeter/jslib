ff6de3772aa01e5ff919c3eb2e690518884e26a5
jeppeter <jeppeter@gmail.com>
2017-12-12 14:06:04 +0800

    to add sse_main.js

---
+++ test/tstszs/sse_main.js
@@ -0,0 +1,19 @@
+var util = require('util');
+var grabwork = require('../../grabwork');
+var grab = grabwork();
+var jstracer = require('jstracer');
+
+function createSSEMainRequest(opt, worker, idx) {
+
+}
+
+function addSSEMainCode(options, stockcode) {
+
+}
+
+function createSSEMain(options) {
+
+}
+
+modules.export = createSSEMain;
+modules.export.addSSEMainCode = addSSEMainCode;
