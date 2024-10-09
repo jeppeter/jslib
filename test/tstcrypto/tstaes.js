30588ee9b1ea8c766e62bcaf6d7298441a99adf0
jeppeter <jeppeter@gmail.com>
2024-10-09 09:31:47 +0800

    tstaes.js with coding

--- test/tstcrypto/tstaes.js
+++ test/tstcrypto/tstaes.js
@@ -4,9 +4,15 @@ var stime = CryptoJS.enc.Utf8.parse(Math.floor(dtime));
 var keystr = CryptoJS.enc.Utf8.parse('1234567887654321');
 var encdata = CryptoJS.AES.encrypt(stime, keystr, {iv: keystr, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
 var encbase64 = CryptoJS.enc.Base64.stringify(encdata.ciphertext);
+var nstime = CryptoJS.enc.Base64.stringify(stime);
+var decstime = CryptoJS.enc.Base64.parse(nstime);
 //var skey = '';
 stime.words.forEach(function (c) {
     'use strict';
     console.log('c %s', c);
 });
-console.log('stime %s encbase64 %s', stime, encbase64);
+decstime.words.forEach(function (c) {
+    'use strict';
+    console.log('dec c %s', c);
+});
+console.log('stime %s encbase64 %s\nnstime %s', stime, encbase64, nstime);
