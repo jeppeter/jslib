439a32c602952c3b0c24c01b117a3cca4afdaf6a
jeppeter <jeppeter@gmail.com>
2024-10-09 13:02:34 +0800

    to add tstsel.js

--- test/tstcrypto/public.sh
+++ test/tstcrypto/public.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-curl -vvvvv  -d 'sdate=20241009&edate=20241009' -o out.json \
---header 'Accept: */*' --header 'Accept-EncKey: 7i7Jslm8QKMZGwn+Ri4yYA==' \
+curl -vvvvv  -o out.json \
+--header 'Accept: */*' --header 'Accept-EncKey: kBHIDgu19LPA6iRcLqiLgQ==' \
 --header 'Accept-Encoding: gzip, deflate' --header 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' \
 --header 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
 --header 'Host: webapi.cninfo.com.cn' \
@@ -9,4 +9,4 @@ curl -vvvvv  -d 'sdate=20241009&edate=20241009' -o out.json \
 --header 'Referer: http://webapi.cninfo.com.cn/' \
 --header 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36' \
 --header 'X-Requested-With: XMLHttpRequest' \
-http://webapi.cninfo.com.cn/api/stock/p_public0001
+http://webapi.cninfo.com.cn/api/info/p_info3085?scode=300511&sdate=20200101&edata=20240101
---
+++ test/tstcrypto/tstsel.js
@@ -0,0 +1,110 @@
+var jstracer = require('jstracer');
+var extargsparse = require('extargsparse');
+var baseop = require('../../baseop');
+var util = require('util');
+var d = new Date();
+var fs = require('fs');
+
+var curdate = '';
+curdate += baseop.number_format_length(4, d.getFullYear());
+curdate += '-';
+curdate += baseop.number_format_length(2, d.getMonth() + 1);
+curdate += '-';
+curdate += baseop.number_format_length(2, d.getDate());
+
+
+var trace_exit = function (ec) {
+    'use strict';
+    jstracer.finish(function (err) {
+        if (err) {
+            return;
+        }
+        process.exit(ec);
+    });
+    return;
+};
+
+var command_line_format = `
+{
+    "startdate|S" : "2000-01-01",
+    "enddate|E" : "%s",
+    "selpdf<selpdf_handler>##to list pdf select##" :{
+        "$" : "+"
+    }
+}`;
+var command_line;
+var parser;
+var args;
+//var grab = grabwork({});
+parser = extargsparse.ExtArgsParse({
+    help_func: function (ec, s) {
+        'use strict';
+        var fp;
+        if (ec === 0) {
+            fp = process.stdout;
+        } else {
+            fp = process.stderr;
+        }
+        fp.write(s);
+        trace_exit(ec);
+    }
+});
+
+var curdir = __dirname;
+curdir = curdir.replace(/\\/g, '\\\\');
+command_line = util.format(command_line_format, curdate);
+parser.load_command_line_string(command_line);
+jstracer.init_args(parser);
+
+process.on('uncaughtException', function (err) {
+    'use struct';
+    jstracer.error('error (%s) stack(%s)', err, err.stack);
+    trace_exit(3);
+});
+
+process.on('SIGINT', function () {
+    'use strict';
+    trace_exit(3);
+});
+
+process.on('exit', function (coderr) {
+    'use strict';
+    //if (coderr === 0) {
+        //grab.assert_exit_dump();
+    //}
+    trace_exit(coderr);
+});
+
+var selpdf_handler = function (args) {
+    'use strict';
+    var connval = 0;
+    jstracer.set_args(args);
+
+    args.subnargs.forEach(function (elm) {
+        connval += 1;
+        fs.readFile(elm, function (err, data) {
+            if (baseop.is_non_null(err)) {
+                jstracer.error('read %s error %s', elm, err);
+                trace_exit(3);
+                return;
+            }
+            var dv = JSON.parse(data);
+            jstracer.info('dv %s', dv);
+            connval -= 1;
+            if (connval === 0) {
+                trace_exit(0);
+            }
+        });
+    });
+    if (connval === 0) {
+        trace_exit(0);
+    }
+    return;
+};
+
+exports.selpdf_handler = selpdf_handler;
+
+
+args = parser.parse_command_line();
+jstracer.set_args(args);
+
