07dceb1b45aae11d91f1f10c9b09ed90a956163f
jeppeter <jeppeter@gmail.com>
2025-04-21 17:17:27 +0800

    to make hk2 stockid

---
+++ test/tsthkex/hk2_stockid.js
@@ -0,0 +1,200 @@
+var jstracer = require('jstracer');
+var baseop = require('../../baseop');
+var grabwork = require('../../grabwork');
+var path = require('path');
+
+
+function createHk2StockId(options) {
+    'use strict';
+    var hk2stockid;
+    hk2stockid = {};
+    hk2stockid = {};
+    hk2stockid.options.startdate = '20000101';
+    d = new Date();
+    hk2stockid.options.enddate = '';
+    cninfo.options.enddate += baseop.number_format_length(4, d.getFullYear());
+    cninfo.options.enddate += '-';
+    cninfo.options.enddate += baseop.number_format_length(2, d.getMonth() + 1);
+    cninfo.options.enddate += '-';
+    cninfo.options.enddate += baseop.number_format_length(2, d.getDate());
+    
+
+
+    if (baseop.is_valid_date_ex(options.startdate)) {
+        hk2stockid.options.startdate = options.startdate;
+    }
+
+    if (baseop.is_valid_date_ex(options.enddate)) {
+        hk2stockid.options.enddate = options.enddate;
+    }
+
+    if (baseop.is_valid_string(options, 'topdir', 1)) {
+        cninfo.options.baselocate = options.topdir;
+    }
+
+    if (baseop.is_valid_number(options.timeout, false)) {
+        cninfo.options.timeout = options.timeout;
+    }
+
+
+
+    cninfo.post_next_error = function (err, worker, next) {
+        jstracer.error('<GET::%s> error %s', worker.url, err);
+        worker.reqopt.cninfomain.trycnt += 1;
+        if (worker.reqopt.cninfomain.trycnt < worker.reqopt.cninfomain.maxcnt) {
+            var url;
+            var headers = {};
+            url = cninfo.format_url(worker.reqopt.cninfo.stockcode);
+            headers = cninfo.get_headers();
+            worker.parent.queue(url, {
+                reqopt: {
+                    timeout: cninfo.options.timeout,
+                    headers: headers
+                },
+                priority: grabwork.MIN_PRIORITY,
+                cninfomain: worker.reqopt.cninfomain
+            });
+        }
+        next(false, err);
+        return;
+    };
+
+    cninfo.hyfein_date_to_value = function (hdate) {
+        var retval = 0;
+        var sarr = hdate.split('-');
+        if (sarr.length >= 3) {
+            retval += parseInt(sarr[0], 10) * 10000;
+            retval += parseInt(sarr[1], 10) * 100;
+            retval += parseInt(sarr[2], 10);
+        }
+        return retval;
+    };
+
+    cninfo.parse_urls = function (dv) {
+        var returls = [];
+        var svalue = cninfo.hyfein_date_to_value(cninfo.options.startdate);
+        var evalue = cninfo.hyfein_date_to_value(cninfo.options.enddate);
+        if (baseop.is_non_null(dv, 'records')) {
+            dv.records.forEach(function (cv) {
+                if (baseop.is_non_null(cv, 'F003V')) {
+                    var ccarr = cv.F003V.split('/');
+                    if (ccarr.length >= 2) {
+                        var cval = cninfo.hyfein_date_to_value(ccarr[ccarr.length - 2]);
+                        if (cval >= svalue && cval <= evalue) {
+                            returls.push(cv.F003V);
+                        }
+                    }
+                } else {
+                    jstracer.info('no F003V');
+                }
+            });
+        } else {
+            jstracer.info('no records');
+        }
+        return returls;
+    };
+
+    cninfo.post_handler = function (err, worker, next) {
+
+
+        if (!baseop.is_non_null(worker.reqopt.cninfomain)) {
+            next(true, err);
+            return;
+        }
+
+        if (err) {
+            /*we should query again*/
+            cninfo.post_next_error(err, worker, next);
+            return;
+        }
+        /*to parse data*/
+        try {
+            var dv = JSON.parse(worker.htmldata);
+            var returls = cninfo.parse_urls(dv);
+            returls.forEach(function (cv) {
+                /*to get the date*/
+                var downloadurl;
+                var downloadreqopt = {};
+                var yearnum = '2020';
+                var fname;
+                var sarr;
+                downloadurl = cv;
+                sarr = downloadurl.split('/');
+                if (sarr.length >= 2) {
+                    var carr = sarr[sarr.length - 2].split('-');
+                    if (carr.length > 0) {
+                        yearnum = carr[0];
+                    }
+                }
+
+                fname = path.join(cninfo.options.baselocate, worker.reqopt.cninfomain.stockcode, yearnum, sarr[sarr.length - 1]);
+                downloadreqopt.downloadoption = {};
+                downloadreqopt.downloadoption.downloadfile = fname;
+                grab.download_queue(downloadurl, downloadreqopt);
+            });
+
+
+        } catch (e) {
+            jstracer.error('e %s', e);
+            cninfo.post_next_error(e, worker, next);
+            return;
+        }
+
+
+        /*ok ,we should have this*/
+        next(false, null);
+        return;
+    };
+
+
+    cninfo.format_url = function (stockcode) {
+        return util.format('http://webapi.cninfo.com.cn/api/info/p_info3085?scode=%s', stockcode);
+    };
+
+    cninfo.get_cninfo_scode = function () {
+        var dtime = (new Date().getTime() / 1000);
+        var stime = CryptoJS.enc.Utf8.parse(Math.floor(dtime));
+        var keystr = CryptoJS.enc.Utf8.parse('1234567887654321');
+        var encdata = CryptoJS.AES.encrypt(stime, keystr, {iv: keystr, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
+        return CryptoJS.enc.Base64.stringify(encdata.ciphertext);
+    };
+
+    cninfo.get_headers = function () {
+        var headers = {};
+        headers.Accept = '*/*';
+        headers['Accept-EncKey'] = cninfo.get_cninfo_scode();
+        jstracer.trace('Accept-EncKey %s', headers['Accept-EncKey']);
+        //headers['Accept-Encoding'] = 'gzip, deflate';
+        headers['Accept-Language'] = 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7';
+        headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+        headers.Host = 'webapi.cninfo.com.cn';
+        headers.Origin = 'http://webapi.cninfo.com.cn';
+        headers.Referer = 'http://webapi.cninfo.com.cn/';
+        headers['X-Requested-With'] = 'XMLHttpRequest';
+        return headers;
+    };
+
+    cninfo.post_queue_url = function (stockcode) {
+        var url;
+        var headers = {};
+        url = cninfo.format_url(stockcode);
+        headers = cninfo.get_headers();
+        grab.queue(url, {
+            reqopt: {
+                timeout: cninfo.options.timeout,
+                headers: headers
+            },
+            cninfomain: {
+                stockcode: stockcode,
+                enddate: cninfo.options.enddate,
+                startdate: cninfo.options.startdate,
+                trycnt: 0,
+                maxcnt: cninfo.options.maxcnt
+            }
+        });
+    };
+
+    return cninfo;
+}
+
+module.exports = createCninfoNewMain;
---
+++ test/tsthkex/tsthk2.js
@@ -0,0 +1,92 @@
+var jstracer = require('jstracer');
+var grabwork = require('../../grabwork');
+var baseop = require('../../baseop');
+var util = require('util');
+//var util = require('util');
+var grab = grabwork();
+var download_pre = require('../../grabwork/download_pre');
+var random_delay = require('../../grabwork/random_delay');
+var extargsparse = require('extargsparse');
+var curdate;
+var d = new Date();
+
+curdate = '';
+curdate += baseop.number_format_length(4, d.getFullYear());
+curdate += baseop.number_format_length(2, d.getMonth() + 1);
+curdate += baseop.number_format_length(2, d.getDate());
+
+
+
+var trace_exit = function (ec) {
+    'use strict';
+    jstracer.finish(function (err) {
+        if (err) {
+            return;
+        }
+        process.exit(ec);
+    });
+    return;
+};
+
+
+var command_line_format = `
+    {
+        "grabmaxsock|m" : 10,
+        "grabtimeout|t" : 10000,
+        "startdate|S" : "19990101",
+        "enddate|E" : "%s",
+        "stockcode|s" : "02010",
+        "topdir|P" : "%s",
+        "watermark|w" : 20,
+        "url|U" : "http://www.hkexnews.hk/listedco/listconews/advancedsearch/search_active_main_c.aspx"
+    }
+`;
+var command_line;
+var parser;
+var args;
+
+parser = extargsparse.ExtArgsParse({
+    help_func: function (ec, s) {
+        'use strict';
+        var fp;
+        if (ec === 0) {
+            fp = process.stdout;
+        } else {
+            fp = process.stderr;
+        }
+        fp.write(s);
+        trace_exit(ec);
+    }
+});
+
+var curdir = __dirname;
+curdir = curdir.replace(/\\/g, '\\\\');
+command_line = util.format(command_line_format, curdate, curdir);
+parser.load_command_line_string(command_line);
+jstracer.init_args(parser);
+
+process.on('uncaughtException', function (err) {
+    'use struct';
+    jstracer.error('error (%s) stack(%s)', err, err.stack);
+    trace_exit(3);
+});
+
+process.on('SIGINT', function () {
+    'use strict';
+    trace_exit(0);
+});
+
+args = parser.parse_command_line();
+jstracer.set_args(args);
+
+
+grab.add_pre(random_delay(args));
+grab.add_pre(download_pre(args));
+
+
+grab.queue(args.url, {
+    hkexnewsmainoption: {},
+    reqopt: {
+        timeout: args.grabtimeout
+    }
+});
