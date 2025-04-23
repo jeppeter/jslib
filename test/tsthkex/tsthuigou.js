8569a9cbd084cdd245af5c01a520f9c94bdc46ef
jeppeter <jeppeter@gmail.com>
2025-04-23 11:31:37 +0800

    to add tsthuigou

---
+++ test/tsthkex/tsthuigou.js
@@ -0,0 +1,294 @@
+/*
+POST https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/sbn_c.asp
+data
+y=2025&m=3
+return
+htmlfile like
+<div class='day-grid'>
+   <table>
+      <thead>
+         <tr>
+            <th class='cal-day-header cal-sun'>星期日</th>
+            <th class='cal-day-header cal-mon'>星期一</th>
+            <th class='cal-day-header cal-tue'>星期二</th>
+            <th class='cal-day-header cal-wed'>星期三</th>
+            <th class='cal-day-header cal-thu'>星期四</th>
+            <th class='cal-day-header cal-fri'>星期五</th>
+            <th class='cal-day-header cal-sat'>星期六</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr>
+            <td class='cal-day cal-sun cal-no-report'>-</td>
+            <td class='cal-day cal-mon cal-no-report'>-</td>
+            <td class='cal-day cal-tue cal-no-report'>-</td>
+            <td class='cal-day cal-wed cal-no-report'>-</td>
+            <td class='cal-day cal-thu cal-no-report'>-</td>
+            <td class='cal-day cal-fri cal-no-report'>-</td>
+            <td class='cal-day cal-no-report cal-sat'>-</td>
+         </tr>
+         <tr>
+            <td class='cal-day cal-no-report cal-sun'>-</td>
+            <td class='cal-day cal-mon'>
+               <div class='report-context'>
+                  <div class='report-date'>3
+                     <span class='fileicon xls'></span>
+                  </div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250303.xls' target=_blank class='report-download-overlay'>
+               <span class='download'></span>
+               <span>下载报告</span>
+               </a>
+            </td>
+            <td class='cal-day cal-tue'>
+               <div class='report-context'>
+                  <div class='report-date'>4
+                     <span class='fileicon xls'></span>
+                  </div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250304.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-wed'>
+               <div class='report-context'>
+                  <div class='report-date'>5<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250305.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-thu'>
+               <div class='report-context'>
+                  <div class='report-date'>6<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250306.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-fri'>
+               <div class='report-context'>
+                  <div class='report-date'>7<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250307.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-no-report cal-sat'>-</td>
+         </tr>
+         <tr>
+            <td class='cal-day cal-no-report cal-sun'>-</td>
+            <td class='cal-day cal-mon'>
+               <div class='report-context'>
+                  <div class='report-date'>10<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250310.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-tue'>
+               <div class='report-context'>
+                  <div class='report-date'>11<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250311.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-wed'>
+               <div class='report-context'>
+                  <div class='report-date'>12<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250312.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-thu'>
+               <div class='report-context'>
+                  <div class='report-date'>13<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250313.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-fri'>
+               <div class='report-context'>
+                  <div class='report-date'>14<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250314.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-no-report cal-sat'>-</td>
+         </tr>
+         <tr>
+            <td class='cal-day cal-no-report cal-sun'>-</td>
+            <td class='cal-day cal-mon'>
+               <div class='report-context'>
+                  <div class='report-date'>17<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250317.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-tue'>
+               <div class='report-context'>
+                  <div class='report-date'>18<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250318.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-wed'>
+               <div class='report-context'>
+                  <div class='report-date'>19<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250319.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-thu'>
+               <div class='report-context'>
+                  <div class='report-date'>20<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250320.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-fri'>
+               <div class='report-context'>
+                  <div class='report-date'>21<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250321.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-no-report cal-sat'>-</td>
+         </tr>
+         <tr>
+            <td class='cal-day cal-no-report cal-sun'>-</td>
+            <td class='cal-day cal-mon'>
+               <div class='report-context'>
+                  <div class='report-date'>24<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250324.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-tue'>
+               <div class='report-context'>
+                  <div class='report-date'>25<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250325.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-wed'>
+               <div class='report-context'>
+                  <div class='report-date'>26<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250326.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-thu'>
+               <div class='report-context'>
+                  <div class='report-date'>27<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250327.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-fri'>
+               <div class='report-context'>
+                  <div class='report-date'>28<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250328.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-no-report cal-sat'>-</td>
+         </tr>
+         <tr>
+            <td class='cal-day cal-no-report cal-sun'>-</td>
+            <td class='cal-day cal-mon'>
+               <div class='report-context'>
+                  <div class='report-date'>31<span class='fileicon xls'></span></div>
+               </div>
+               <a href='https://sc.hkexnews.hk/TuniS/www3.hkexnews.hk/reports/sharerepur/documents/SRRPT20250331.xls' target=_blank class='report-download-overlay'><span class='download'></span><span>下载报告</span></a>
+            </td>
+            <td class='cal-day cal-no-report cal-tue cal-future'>-</td>
+            <td class='cal-day cal-no-report cal-wed cal-future'>-</td>
+            <td class='cal-day cal-no-report cal-thu cal-future'>-</td>
+            <td class='cal-day cal-no-report cal-fri cal-future'>-</td>
+            <td class='cal-day cal-no-report cal-sat cal-future'>-</td>
+         </tr>
+      </tbody>
+   </table>
+</div>
+
+
+get "div[class='day-grid'] a" attribute href will get the file
+
+
+*/
+
+
+var jstracer = require('jstracer');
+var grabwork = require('../../grabwork');
+var baseop = require('../../baseop');
+var util = require('util');
+//var util = require('util');
+var grab = grabwork();
+var download_pre = require('../../grabwork/download_pre');
+var random_delay = require('../../grabwork/random_delay');
+var extargsparse = require('extargsparse');
+var hk2stockid = require('./hk2_stockid');
+var hk2list = require('./hk2_list');
+var curdate;
+var d = new Date();
+
+curdate = '';
+curdate += baseop.number_format_length(4, d.getFullYear());
+curdate += baseop.number_format_length(2, d.getMonth() + 1);
+curdate += baseop.number_format_length(2, d.getDate());
+
+
+
+var trace_exit = function (ec) {
+    'use strict';
+    jstracer.finish(function (err) {
+        if (err) {
+            return;
+        }
+        process.exit(ec);
+    });
+    return;
+};
+
+
+var command_line_format = `
+    {
+        "grabmaxsock|m" : 10,
+        "grabtimeout|t" : 10000,
+        "startdate|S" : "19990101",
+        "downloadmax|M" : 5,
+        "enddate|E" : "%s",
+        "stockcode|s" : "02010",
+        "topdir|P" : "%s",
+        "watermark|w" : 20,
+        "url|U" : "http://www.hkexnews.hk/listedco/listconews/advancedsearch/search_active_main_c.aspx"
+    }
+`;
+var command_line;
+var parser;
+var args;
+
+parser = extargsparse.ExtArgsParse({
+    help_func: function (ec, s) {
+        'use strict';
+        var fp;
+        if (ec === 0) {
+            fp = process.stdout;
+        } else {
+            fp = process.stderr;
+        }
+        fp.write(s);
+        trace_exit(ec);
+    }
+});
+
+var curdir = __dirname;
+curdir = curdir.replace(/\\/g, '\\\\');
+command_line = util.format(command_line_format, curdate, curdir);
+parser.load_command_line_string(command_line);
+jstracer.init_args(parser);
+
+process.on('uncaughtException', function (err) {
+    'use struct';
+    jstracer.error('error (%s) stack(%s)', err, err.stack);
+    trace_exit(3);
+});
+
+process.on('SIGINT', function () {
+    'use strict';
+    trace_exit(0);
+});
+
+args = parser.parse_command_line();
+jstracer.set_args(args);
+
+
+grab.add_pre(random_delay(args));
+grab.add_pre(download_pre(args));
+var idproc = hk2stockid(args);
+var hk2proc = hk2list(args);
+grab.add_post(idproc);
+grab.add_post(hk2proc);
+
+
+args.args.forEach(function (elm) {
+    'use strict';
+    idproc.start_code(elm);
+});
